efc5c820eb89384ac6b3b15ab0811e73aed59987
Anton Frolov <afrolov@lockobank.ru>
2025-07-10 21:04:47 +0300

    Initial Commit

---
+++ BluetoothMessenger/BluetoothMessengerApp.swift
@@ -0,0 +1,32 @@
+//
+//  BluetoothMessengerApp.swift
+//  BluetoothMessenger
+//
+//  Created by Аркадий Мэй on 10.07.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BluetoothMessengerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
